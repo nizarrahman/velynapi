72a23305edd20e0173275977be77972c5538e307
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2025-03-29 08:52:48 +0700

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Velyn</div>
+      <div className={styles.logo}>NV Rest Api</div>
       <div className={styles.text}>
         NV Team © All rights reserved 2025.
       </div>
